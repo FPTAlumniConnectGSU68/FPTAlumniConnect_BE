70f4751836ce36543ca4d050e324ef225f308503
ThinhNguyen2304 <vippro2304@gmail.com>
2025-07-24 18:26:36 +0700

    Cleanup code

--- FPTAlumniConnect.DataTier/Models/EventTimeLine.cs
+++ FPTAlumniConnect.DataTier/Models/EventTimeLine.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FPTAlumniConnect.DataTier.Models
 {
@@ -11,8 +6,8 @@ namespace FPTAlumniConnect.DataTier.Models
     {
         [Key]
         public int EventTimeLineId { get; set; }
-        public int EventId {  get; set; }
-        public string? Title {  get; set; }
+        public int EventId { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
