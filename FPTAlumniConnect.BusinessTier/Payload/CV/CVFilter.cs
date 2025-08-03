3cfdce694209e78a548e8d95f515ac9490d6df30
ACER <phidoan5153@gmail.com>
2025-08-03 13:33:45 +0700

    update: fix get all cv

--- FPTAlumniConnect.BusinessTier/Payload/CV/CVFilter.cs
+++ FPTAlumniConnect.BusinessTier/Payload/CV/CVFilter.cs
@@ -1,6 +1,7 @@
-﻿using System.Linq.Expressions;
+﻿using FPTAlumniConnect.BusinessTier.Utils;
 using FPTAlumniConnect.DataTier.Enums;
 using FPTAlumniConnect.DataTier.Models;
+using System.Linq.Expressions;
 
 namespace FPTAlumniConnect.BusinessTier.Payload.CV
 {
@@ -21,151 +22,46 @@ namespace FPTAlumniConnect.BusinessTier.Payload.CV
 
         public Expression<Func<Cv, bool>> BuildPredicate()
         {
-            Expression<Func<Cv, bool>> predicate = x => true;
+            var predicate = PredicateBuilder.True<Cv>();
 
             if (StartAt.HasValue)
-            {
-                var startAtPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.GreaterThanOrEqual(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.StartAt)),
-                        Expression.Constant(StartAt.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, startAtPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.StartAt >= StartAt.Value);
 
             if (EndAt.HasValue)
-            {
-                var endAtPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.LessThanOrEqual(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.EndAt)),
-                        Expression.Constant(EndAt.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, endAtPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.EndAt <= EndAt.Value);
 
             if (!string.IsNullOrEmpty(Status))
             {
-                var statusPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.Status)),
-                        Expression.Constant(Enum.Parse<CVStatus>(Status))),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, statusPredicate.Body),
-                    predicate.Parameters);
+                if (Enum.TryParse<CVStatus>(Status, out var parsedStatus))
+                    predicate = predicate.And(x => x.Status == parsedStatus);
             }
 
             if (UserId.HasValue)
-            {
-                var userIdPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.UserId)),
-                        Expression.Constant(UserId.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, userIdPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.UserId == UserId.Value);
 
             if (!string.IsNullOrEmpty(City))
-            {
-                var cityPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.City)),
-                        Expression.Constant(City)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, cityPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.City == City);
 
             if (!string.IsNullOrEmpty(JobLevel))
-            {
-                var jobLevelPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.JobLevel)),
-                        Expression.Constant(JobLevel)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, jobLevelPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.JobLevel == JobLevel);
 
             if (MinSalary.HasValue)
-            {
-                var minSalaryPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.GreaterThanOrEqual(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.MinSalary)),
-                        Expression.Constant(MinSalary.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, minSalaryPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.MinSalary >= MinSalary.Value);
 
             if (MaxSalary.HasValue)
-            {
-                var maxSalaryPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.LessThanOrEqual(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.MaxSalary)),
-                        Expression.Constant(MaxSalary.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, maxSalaryPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.MaxSalary <= MaxSalary.Value);
 
             if (IsDeal.HasValue)
-            {
-                var isDealPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.IsDeal)),
-                        Expression.Constant(IsDeal.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, isDealPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.IsDeal == IsDeal.Value);
 
             if (!string.IsNullOrEmpty(DesiredJob))
-            {
-                var desiredJobPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.DesiredJob)),
-                        Expression.Constant(DesiredJob)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, desiredJobPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.DesiredJob == DesiredJob);
 
             if (!string.IsNullOrEmpty(Position))
-            {
-                var positionPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.Position)),
-                        Expression.Constant(Position)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, positionPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.Position == Position);
 
             if (MajorId.HasValue)
-            {
-                var majorIdPredicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.Equal(
-                        Expression.Property(Expression.Parameter(typeof(Cv), "x"), nameof(Cv.MajorId)),
-                        Expression.Constant(MajorId.Value)),
-                    Expression.Parameter(typeof(Cv), "x"));
-                predicate = Expression.Lambda<Func<Cv, bool>>(
-                    Expression.AndAlso(predicate.Body, majorIdPredicate.Body),
-                    predicate.Parameters);
-            }
+                predicate = predicate.And(x => x.MajorId == MajorId.Value);
 
             return predicate;
         }
